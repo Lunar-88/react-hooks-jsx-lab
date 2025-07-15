ff3d3671942e826b41a5c968988d32056a00e7c7
Felix Kemboi <felixkemboi824@gmail.com>
2025-07-15 06:50:34 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  
+  return (
+     <div id="home">
+      <h1>{name} is a Web developer from {city}</h1>
+      Home
+    </div>
+  )
 }
 
 export default Home;
